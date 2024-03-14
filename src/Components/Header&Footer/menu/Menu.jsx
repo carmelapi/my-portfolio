905596570ca42919e5cb8d17c38a7a76db79cc33
Pippa <carmela.pippa@hyperisland.se>
2024-03-14 10:53:43 +0100

    Changed menu

--- src/Components/Header&Footer/menu/Menu.jsx
+++ src/Components/Header&Footer/menu/Menu.jsx
@@ -3,9 +3,9 @@ export default function ShowMenu() {
   return (
     <div className="menu__container">
       <ul className="menu__list">
-        <li> Project</li>
-        <li> Career</li>
-        <li> Contact</li>
+        <li>Project</li>
+        <li>Career</li>
+        <li>Contact</li>
       </ul>
     </div>
   );
