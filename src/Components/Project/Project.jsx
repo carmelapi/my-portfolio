d3d8788c34b1731cb5a2b4920435b3f7f8b0354e
Pippa <carmela.pippa@hyperisland.se>
2024-03-15 11:31:29 +0100

    fixed typo error

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -2,7 +2,7 @@ export default function Project() {
   return (
     <div style={{ textAlign: "center" }}>
       This site is still under construction, <br /> if you want to see some of
-      my old projects,{" "}
+      my old project,{" "}
       <a
         href="https://www.figma.com/proto/MBwomhhdgiyFQQfhiRBIxgIg/Pippa?page-id=1400%3A1296&node-id=1400-1488&mode=design&t=JNwaWuEPC1F3hN4P-1"
         target="_blank"
